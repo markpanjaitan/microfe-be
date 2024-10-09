cfbd7e8d06a78f5b3ee6d4e8bb89f5c15b0476b6
ATRAPA\marvalentino <marvalentino@deloitte.com>
2024-10-10 00:01:02 +0700

    first commit

---
+++ tasklist-service/src/main/java/com/deloitte/TasklistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistServiceApplication.class, args);
+	}
+
+}
